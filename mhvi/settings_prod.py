5a5c66474b403a4c65991a9c1bf85355da0c3664
rsperko <circular_file-github@sperko.com>
2014-03-20 07:55:11 -0500

    add heroku support

--- mhvi/settings_prod.py
+++ mhvi/settings_prod.py
@@ -16,7 +16,9 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
