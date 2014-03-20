49ee6bb0e654de35d7a72f8347aa1fff52f12bce
rsperko <circular_file-github@sperko.com>
2014-03-20 07:52:52 -0500

    add heroku support

---
+++ mhvi/settings_prod.py
@@ -0,0 +1,27 @@
+"""
+Django settings for mhvi project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+from settings_common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+IN_PROD = True
