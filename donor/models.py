83acf749749f1c2ead119c77a52d6afb35a3c338
rsperko <circular_file-github@sperko.com>
2014-03-20 15:28:48 -0500

    name clean up of constants

--- donor/models.py
+++ donor/models.py
@@ -3,56 +3,58 @@ import datetime
 
 # Create your models here.
 class Donor(models.Model):
-    DONOR = 'D'
-    PARTNER = 'P'
-    DONOR_TYPES = (
-        (DONOR, 'Donor'),
-        (PARTNER, 'Partner')
+    TYPE_DONOR = 'D'
+    TYPE_PARTNER = 'P'
+    TYPES = (
+        (TYPE_DONOR, 'Donor'),
+        (TYPE_PARTNER, 'Partner')
     )
     CATEGORY_INDIVIDUAL = 'I'
     CATEGORY_BUSINESS = 'B'
     CATEGORY_NON_PROFIT = 'N'
     CATEGORY_FOUNDATION = 'F'
+    CATEGORY_OTHER = 'O'
     CATEGORIES = (
         (CATEGORY_BUSINESS, 'Business'),
         (CATEGORY_FOUNDATION, 'Foundation'),
         (CATEGORY_INDIVIDUAL, 'Individual'),
-        (CATEGORY_NON_PROFIT, 'Non-Profit')
+        (CATEGORY_NON_PROFIT, 'Non-Profit'),
+        (CATEGORY_OTHER, 'Other - see notes')
     )
     added = models.DateField(auto_now_add=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     institution_name = models.CharField(max_length=100, blank=True)
-    type = models.CharField(max_length=1,
-                            choices=DONOR_TYPES,
-                            default=DONOR)
     # category = models.CharField(max_length=1,
     #                             choices=CATEGORIES,
     #                             default=CATEGORY_INDIVIDUAL)
     email = models.EmailField()
     notes = models.TextField()
+    type = models.CharField(max_length=1,
+                            choices=TYPES,
+                            default=TYPE_DONOR)
 
 class Phone(models.Model):
-    HOME = 'H'
-    MOBILE = 'M'
-    WORK = 'W'
-    OTHER = 'O'
-    PHONE_TYPES = (
-        (HOME, 'Home'),
-        (MOBILE, 'Mobile'),
-        (WORK, 'Work'),
-        (OTHER, 'Other')
+    TYPE_HOME = 'H'
+    TYPE_MOBILE = 'M'
+    TYPE_WORK = 'W'
+    TYPE_OTHER = 'O'
+    TYPES = (
+        (TYPE_HOME, 'Home'),
+        (TYPE_MOBILE, 'Mobile'),
+        (TYPE_WORK, 'Work'),
+        (TYPE_OTHER, 'Other')
     )
     donor = models.ForeignKey(Donor)
     preferred = models.BooleanField()
     number = models.CharField(max_length=12)
     type = models.CharField(max_length=1,
-                            choices=PHONE_TYPES,
-                            default=MOBILE)
+                            choices=TYPES,
+                            default=TYPE_MOBILE)
 
 class Address(models.Model):
     donor = models.ForeignKey(Donor)
-    preferred = models.BooleanField('preferred address')
+    preferred = models.BooleanField()
     care_of = models.CharField(max_length=100)
     line1 = models.CharField(max_length=100)
     line2 = models.CharField(max_length=100)
@@ -61,23 +63,24 @@ class Address(models.Model):
     postalCode = models.CharField(max_length=10)
 
 class Donation(models.Model):
-    FURNITURE = 'F'
-    CLOTHING = 'C'
-    HOUSEHOLD_ITEM = 'H'
-    FOOD = 'D'
-    MONEY = 'M'
-    OTHER = 'O'
-    DONATION_TYPES = (
-        (CLOTHING, 'Clothing'),
-        (FOOD, 'Food'),
-        (FURNITURE, 'Furniture'),
-        (HOUSEHOLD_ITEM, 'Household Items'),
-        (MONEY, 'Money'),
-        (OTHER, 'Other')
+    TYPE_FURNITURE = 'F'
+    TYPE_CLOTHING = 'C'
+    TYPE_HOUSEHOLD_ITEM = 'H'
+    TYPE_FOOD = 'D'
+    TYPE_MONEY = 'M'
+    TYPE_OTHER = 'O'
+    TYPES = (
+        (TYPE_CLOTHING, 'Clothing'),
+        (TYPE_FOOD, 'Food'),
+        (TYPE_FURNITURE, 'Furniture'),
+        (TYPE_HOUSEHOLD_ITEM, 'Household Items'),
+        (TYPE_MONEY, 'Money'),
+        (TYPE_OTHER, 'Other - see notes')
     )
     donor = models.ForeignKey(Donor)
     date = models.DateField(default=datetime.date.today)
-    monetary_amount = models.DecimalField(decimal_places=2, max_digits=8)
+    monetary_amount = models.DecimalField(decimal_places=2,
+                                          max_digits=8)
     in_kind = models.BooleanField()
     # type = models.CharField(max_length=1,
     #                         choices=DONATION_TYPES,
@@ -85,17 +88,19 @@ class Donation(models.Model):
     notes = models.TextField()
 
 class DonorContact(models.Model):
-    PHONE = 'P'
-    EMAIL = 'E'
-    IN_PERSON = 'I'
-    CONTACT_TYPES = (
-        (EMAIL, 'E-mail'),
-        (IN_PERSON, 'In-Person'),
-        (PHONE, 'Phone')
+    TYPE_PHONE = 'P'
+    TYPE_EMAIL = 'E'
+    TYPE_IN_PERSON = 'I'
+    TYPE_OTHER = 'O'
+    TYPES = (
+        (TYPE_EMAIL, 'E-mail'),
+        (TYPE_IN_PERSON, 'In-Person'),
+        (TYPE_PHONE, 'Phone'),
+        (TYPE_OTHER, 'Other - see notes')
     )
     donor = models.ForeignKey(Donor)
     date_time = models.DateTimeField(default=datetime.datetime.now())
     type = models.CharField(max_length=1,
-                            choices=CONTACT_TYPES,
-                            default=PHONE)
+                            choices=TYPES,
+                            default=TYPE_PHONE)
     notes = models.TextField()
