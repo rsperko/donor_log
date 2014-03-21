61910be39f7763d6cdcc95ecd79eef04a65b3ab7
rsperko <circular_file-github@sperko.com>
2014-03-21 07:05:45 -0500

    starting the page support

--- donor/admin.py
+++ donor/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
 from donor.models import Donor, Phone, Address, Donation, DonorContact
 
-class PhoneInline(admin.StackedInline):
+class PhoneInline(admin.TabularInline):
     model = Phone
     extra = 0
 
-class AddressInline(admin.StackedInline):
+class AddressInline(admin.TabularInline):
     model = Address
     extra = 0
 
-class DonationInline(admin.StackedInline):
+class DonationInline(admin.TabularInline):
     model = Donation
     extra = 0
 
-class DonorContactInline(admin.StackedInline):
+class DonorContactInline(admin.TabularInline):
     model = DonorContact
     extra = 0
 
--- mhvi/urls.py
+++ mhvi/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'mhvi.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^donors/', include('donor.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
