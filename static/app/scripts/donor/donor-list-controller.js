667585a77f528e4498c395f85f164f7c4c68ee53
rsperko <circular_file-github@sperko.com>
2014-04-09 21:59:33 -0500

    jslint cleanup

--- static/app/scripts/donor/donor-list-controller.js
+++ static/app/scripts/donor/donor-list-controller.js
@@ -3,45 +3,45 @@
 angular.module('trackingApp')
   .controller('DonorListCtrl', function ($scope, metaDataService, entityResource, entityModel, alertService) {
 
-      var setupModel = function () {
-          $scope.criteria = {
-            first_name: '',
-            last_name: '',
-            notes: '',
-            donor_unset: '0'
-          };
-          $scope.results = [];
-        },
-
-        setupActions = function () {
-          function _buildSubmit() {
-            var submit = _.extend({}, $scope.criteria);
-
-            $scope.results = [];
-
-            return submit;
-          }
-
-          $scope.search = function () {
-            var submit = _buildSubmit();
-
-            entityResource.query(submit).$promise.then(function (result) {
-              _.each(result, function (entity) {
-                $scope.results.push(entityModel(entity.id, entity));
-              });
-            });
-          };
-        },
-
-        init = function (metaData) {
-          alertService.clear();
-
-          $scope.metaData = metaData;
-
-          setupModel(metaData);
-
-          setupActions();
+    var setupModel = function () {
+        $scope.criteria = {
+          first_name: '',
+          last_name: '',
+          notes: '',
+          donor_unset: '0'
         };
+        $scope.results = [];
+      },
 
-      metaDataService.then(init);
-    });
+      setupActions = function () {
+        function _buildSubmit() {
+          var submit = _.extend({}, $scope.criteria);
+
+          $scope.results = [];
+
+          return submit;
+        }
+
+        $scope.search = function () {
+          var submit = _buildSubmit();
+
+          entityResource.query(submit).$promise.then(function (result) {
+            _.each(result, function (entity) {
+              $scope.results.push(entityModel(entity.id, entity));
+            });
+          });
+        };
+      },
+
+      init = function (metaData) {
+        alertService.clear();
+
+        $scope.metaData = metaData;
+
+        setupModel(metaData);
+
+        setupActions();
+      };
+
+    metaDataService.then(init);
+  });
--- static/app/scripts/volunteer/volunteer-list-controller.js
+++ static/app/scripts/volunteer/volunteer-list-controller.js
@@ -3,63 +3,63 @@
 angular.module('trackingApp')
   .controller('VolunteerListCtrl', function ($scope, metaDataService, entityResource, entityModel, alertService) {
 
-      var setupModel = function (metaData) {
-          $scope.skills = {};
-          $scope.criteria = {
-            first_name: '',
-            last_name: '',
-            skills: '',
-            notes: '',
-            volunteer_unset: '0'
-          };
-          $scope.results = [];
-          _.each(metaData.volunteer.skill.types, function (value, key) {
-            $scope.skills[key] = false;
-          });
-        },
-
-        setupActions = function () {
-          var _applyApplicableVolunteerCriteria = function(criteria) {
-            var selectedSkills = [];
-              _.each($scope.skills, function (value, key) {
-                if (value) {
-                  selectedSkills.push(key);
-                }
-              });
-              criteria.skills = selectedSkills.join();
-              return criteria;
-            },
-
-            _buildSubmitCriteria = function() {
-              var criteria = _.extend({}, $scope.criteria);
-
-              $scope.results = [];
-
-              criteria = _applyApplicableVolunteerCriteria(criteria);
-
-              return criteria;
-            };
-
-          $scope.search = function () {
-            var submit = _buildSubmitCriteria();
-
-            entityResource.query(submit).$promise.then(function (result) {
-              _.each(result, function (entity) {
-                $scope.results.push(entityModel(entity.id, entity));
-              });
-            });
-          };
-        },
-
-        init = function (metaData) {
-          alertService.clear();
-
-          $scope.metaData = metaData;
-
-          setupModel(metaData);
-
-          setupActions();
+    var setupModel = function (metaData) {
+        $scope.skills = {};
+        $scope.criteria = {
+          first_name: '',
+          last_name: '',
+          skills: '',
+          notes: '',
+          volunteer_unset: '0'
         };
+        $scope.results = [];
+        _.each(metaData.volunteer.skill.types, function (value, key) {
+          $scope.skills[key] = false;
+        });
+      },
 
-      metaDataService.then(init);
-    });
+      setupActions = function () {
+        var _applyApplicableVolunteerCriteria = function (criteria) {
+            var selectedSkills = [];
+            _.each($scope.skills, function (value, key) {
+              if (value) {
+                selectedSkills.push(key);
+              }
+            });
+            criteria.skills = selectedSkills.join();
+            return criteria;
+          },
+
+          _buildSubmitCriteria = function () {
+            var criteria = _.extend({}, $scope.criteria);
+
+            $scope.results = [];
+
+            criteria = _applyApplicableVolunteerCriteria(criteria);
+
+            return criteria;
+          };
+
+        $scope.search = function () {
+          var submit = _buildSubmitCriteria();
+
+          entityResource.query(submit).$promise.then(function (result) {
+            _.each(result, function (entity) {
+              $scope.results.push(entityModel(entity.id, entity));
+            });
+          });
+        };
+      },
+
+      init = function (metaData) {
+        alertService.clear();
+
+        $scope.metaData = metaData;
+
+        setupModel(metaData);
+
+        setupActions();
+      };
+
+    metaDataService.then(init);
+  });
